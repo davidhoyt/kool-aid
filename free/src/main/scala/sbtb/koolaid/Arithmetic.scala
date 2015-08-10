d271b2bdb32cafed89d18145506e0c03854e573b
David Hoyt <dhoyt@netflix.com>
2015-08-10 16:40:40 -0700

    step:initial

--- free/src/main/scala/sbtb/koolaid/Arithmetic.scala
+++ free/src/main/scala/sbtb/koolaid/Arithmetic.scala
@@ -4,4 +4,16 @@ import sbtb.koolaid.twitter.client.JvmApp
 
 object Arithmetic extends JvmApp {
 
+  //implicit def intToExpr(i: Int): Expr = Const(i)
+
+  //val expression = Add(1, 2)
+
+  //assert(Add(Const(1), Const(2)) == expression)
+  //
+  //println(evaluate(expression))
+  //println(toString(Add(3, Add(4, 5))))
+  //println(toString(Add(Subtract(Add(1, 2), Add(3, 4)), Subtract(Add(5, 6), Add(7, 8)))))
+  //assert(1 == evaluate(Const(1)))
+  //assert((3 + (4 - 5)) == evaluate(Add(3, Subtract(4, 5))))
+  //assert((((1 + 2) - (3 + 4)) + ((5 + 6) - (7 + 8))) == evaluate(Add(Subtract(Add(1, 2), Add(3, 4)), Subtract(Add(5, 6), Add(7, 8)))))
 }
