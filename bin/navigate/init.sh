5458b7049257dbd0277501706284f8e108de40e3
David Hoyt <dhoyt@netflix.com>
2015-08-10 08:55:58 -0700

    step:initial

--- bin/navigate/init.sh
+++ bin/navigate/init.sh
@@ -8,6 +8,8 @@ cd ../..
 
 # initial_commit=$(git log --pretty=format:"%H" --all --grep="$grep_commit_message" | head -n 1)
 
+mkdir -p "/tmp/steps/"
+
 sbt "koan init" > /tmp/steps/last.out
 git clean -fd >> /tmp/steps/last.out
 
