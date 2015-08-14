1996ff226dbdfb5dc5882f67092589823da55b9d
David Hoyt <dhoyt@netflix.com>
2015-08-13 22:25:52 -0700

    working

--- free/src/main/scala/sbtb/koolaid/TwitterDsl.scala
+++ free/src/main/scala/sbtb/koolaid/TwitterDsl.scala
@@ -15,10 +15,10 @@ object TwitterDsl {
   case class GetTweets(screenNames: Seq[String]) extends Instruction[Future[Seq[Tweets]]]
   case class DisplayTweets(tweets: Future[Seq[Tweets]]) extends Instruction[Future[Unit]]
 
-  def tell(text: String, default: String = ""): Free[Instruction, Unit] = ???
-  def ask(default: String): Free[Instruction, String] = ???
+  def tell(text: String, default: String = ""): Free[Instruction, Unit] = Suspend(Tell(text, default))
+  def ask(default: String): Free[Instruction, String] = Suspend(Ask(default))
 
-  def getTweets(screenNames: Seq[String]): Free[Instruction, Future[Seq[Tweets]]] = ???
-  def displayTweets(tweets: Future[Seq[Tweets]]): Free[Instruction, Future[Unit]] = ???
+  def getTweets(screenNames: Seq[String]): Free[Instruction, Future[Seq[Tweets]]] = Suspend(GetTweets(screenNames))
+  def displayTweets(tweets: Future[Seq[Tweets]]): Free[Instruction, Future[Unit]] = Suspend(DisplayTweets(tweets))
 
 }
